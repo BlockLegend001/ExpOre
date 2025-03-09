15ad555abe4424739eaa1375730a733d7186fa36
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 13:49:08 +0100

    Aggiunta 1.21.1-Forge

---
+++ src/main/java/com/blocklegend001/expore/blocks/ModBlocks.java
@@ -0,0 +1,58 @@
+package com.blocklegend001.expore.blocks;
+
+import com.blocklegend001.expore.ExpOre;
+import com.blocklegend001.expore.ExpOreConfig;
+import com.blocklegend001.expore.items.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, ExpOre.MODID);
+
+    public static final RegistryObject<Block> EXP_ORE = registerBlock("exp_ore",
+            () -> new DropExperienceBlock(UniformInt.of(ExpOreConfig.expOreMinExperience, ExpOreConfig.expOreMaxExperience),
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.STONE)
+                            .strength(4f).requiresCorrectToolForDrops()));
+
+    public static final RegistryObject<Block> DEEPSLATE_EXP_ORE = registerBlock("deepslate_exp_ore",
+            () -> new DropExperienceBlock(UniformInt.of(ExpOreConfig.deepslateExpOreMinExperience, ExpOreConfig.deepslateExpOreMaxExperience),
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.STONE)
+                            .strength(4f).requiresCorrectToolForDrops()));
+
+    public static final RegistryObject<Block> NETHER_EXP_ORE = registerBlock("nether_exp_ore",
+            () -> new DropExperienceBlock(UniformInt.of(ExpOreConfig.netherExpOreMinExperience, ExpOreConfig.netherExpOreMaxExperience),
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.STONE)
+                            .strength(4f).requiresCorrectToolForDrops()));
+
+    public static final RegistryObject<Block> END_EXP_ORE = registerBlock("end_exp_ore",
+            () -> new DropExperienceBlock(UniformInt.of(ExpOreConfig.endExpOreMinExperience, ExpOreConfig.endExpOreMaxExperience),
+                    BlockBehaviour.Properties.ofFullCopy(Blocks.STONE)
+                            .strength(4f).requiresCorrectToolForDrops()));
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
