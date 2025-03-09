f1500b24998428f7dc95c4b9bd2634a20259ca78
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 13:48:41 +0100

    Aggiunta 1.20-Forge

---
+++ src/main/java/com/blocklegend001/expore/world/feature/ModPlacedFeatures.java
@@ -0,0 +1,59 @@
+package com.blocklegend001.expore.world.feature;
+
+import com.blocklegend001.expore.ExpOre;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.placement.*;
+
+import java.util.List;
+
+public class ModPlacedFeatures {
+
+    public static final ResourceKey<PlacedFeature> EXP_PLACED_KEY = createKey("exp_placed");
+    public static final ResourceKey<PlacedFeature> NETHER_EXP_PLACED_KEY = createKey("nether_exp_ore_placed");
+    public static final ResourceKey<PlacedFeature> END_EXP_PLACED_KEY = createKey("end_exp_ore_placed");
+
+    public static void bootstrap(BootstapContext<PlacedFeature> context) {
+        HolderGetter<ConfiguredFeature<?, ?>> configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
+
+        register(context, EXP_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.OVERWORLD_EXP_ORE_KEY),
+                commonOrePlacement(12,
+                        HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(-80), VerticalAnchor.absolute(80))));
+
+        register(context, NETHER_EXP_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.NETHER_EXP_ORE_KEY),
+                commonOrePlacement(12,
+                        HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(0), VerticalAnchor.absolute(125))));
+
+        register(context, END_EXP_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.END_EXP_ORE_KEY),
+                commonOrePlacement(12,
+                        HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(0), VerticalAnchor.absolute(255))));
+    }
+
+    public static List<PlacementModifier> orePlacement(PlacementModifier p_195347_, PlacementModifier p_195348_) {
+        return List.of(p_195347_, InSquarePlacement.spread(), p_195348_, BiomeFilter.biome());
+    }
+
+    public static List<PlacementModifier> commonOrePlacement(int p_195344_, PlacementModifier p_195345_) {
+        return orePlacement(CountPlacement.of(p_195344_), p_195345_);
+    }
+
+    private static ResourceKey<PlacedFeature> createKey(String name) {
+        return ResourceKey.create(Registries.PLACED_FEATURE, new ResourceLocation(ExpOre.MODID, name));
+    }
+
+    private static void register(BootstapContext<PlacedFeature> context, ResourceKey<PlacedFeature> key, Holder<ConfiguredFeature<?, ?>> configuration,
+                                 List<PlacementModifier> modifiers) {
+        context.register(key, new PlacedFeature(configuration, List.copyOf(modifiers)));
+    }
+
+    private static void register(BootstapContext<PlacedFeature> context, ResourceKey<PlacedFeature> key, Holder<ConfiguredFeature<?, ?>> configuration,
+                                 PlacementModifier... modifiers) {
+        register(context, key, configuration, List.of(modifiers));
+    }
+}
