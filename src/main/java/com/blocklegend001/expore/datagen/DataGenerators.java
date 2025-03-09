cd70ac8d9581caaebf42562a2fa5254e91ef2ad6
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 13:48:54 +0100

    Aggiunta 1.20.4-Forge

---
+++ src/main/java/com/blocklegend001/expore/datagen/DataGenerators.java
@@ -0,0 +1,23 @@
+package com.blocklegend001.expore.datagen;
+
+import com.blocklegend001.expore.ExpOre;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.concurrent.CompletableFuture;
+
+@Mod.EventBusSubscriber(modid = ExpOre.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        generator.addProvider(event.includeServer(), new ModWorldGenProvider(packOutput, lookupProvider));
+    }
+}
