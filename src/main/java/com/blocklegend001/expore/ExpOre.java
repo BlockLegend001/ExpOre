5b8c9a8200df1273c2ba023b5968fcd16c7a581d
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 13:49:17 +0100

    Aggiunta 1.21.4-Forge

---
+++ src/main/java/com/blocklegend001/expore/ExpOre.java
@@ -0,0 +1,36 @@
+package com.blocklegend001.expore;
+
+import com.blocklegend001.expore.blocks.ModBlocks;
+import com.blocklegend001.expore.items.ModCreativeModTabs;
+import com.blocklegend001.expore.items.ModItems;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(ExpOre.MODID)
+public class ExpOre {
+
+    public static final String MODID = "expore";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ExpOre(FMLJavaModLoadingContext context) {
+        IEventBus modEventBus = context.getModEventBus();
+        ExpOreConfig.loadConfig();
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
+        ModCreativeModTabs.register(modEventBus);
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("ExpOre Loaded");
+    }
+
+}
