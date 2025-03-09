5c0f2e392189da0a99c9020e6f6509c80493115f
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 13:48:37 +0100

    Aggiunta 1.19.4-Forge

---
+++ src/main/java/com/blocklegend001/expore/items/ModCreativeModeTabs.java
@@ -0,0 +1,23 @@
+package com.blocklegend001.expore.items;
+
+import com.blocklegend001.expore.ExpOre;
+import com.blocklegend001.expore.blocks.ModBlocks;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = ExpOre.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModCreativeModeTabs {
+    public static CreativeModeTab EXP_ORE;
+
+    @SubscribeEvent
+    public static void registerCreativeModTabs(CreativeModeTabEvent.Register event) {
+        EXP_ORE = event.registerCreativeModeTab(new ResourceLocation(ExpOre.MODID, "expore"),
+                builder -> builder.icon(() -> new ItemStack(ModBlocks.EXP_ORE.get()))
+                        .title(Component.translatable("itemGroup.expore")));
+    }
+}
