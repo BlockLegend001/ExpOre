80703886a8f3de7815728a5e166246c343d74d30
BlockLegend001 <rosarioarcamone@gmail.com>
2025-06-26 19:09:51 +0200

    Added 1.21.6-Forge

---
+++ src/main/java/com/blocklegend001/expore/datagen/ModLootTables.java
@@ -0,0 +1,18 @@
+package com.blocklegend001.expore.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+public class ModLootTables {
+    public static LootTableProvider create(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> future) {
+        return new LootTableProvider(packOutput, Set.of(),
+                List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK)), future);
+    }
+}
