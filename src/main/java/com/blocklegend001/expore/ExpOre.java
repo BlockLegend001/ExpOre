9799200dea9c983049cbf57cc141b460fd35a48a
BlockLegend001 <rosarioarcamone@gmail.com>
2025-12-18 22:35:31 +0100

    Added 1.21.11-NeoForge

---
+++ src/main/java/com/blocklegend001/expore/ExpOre.java
@@ -0,0 +1,23 @@
+package com.blocklegend001.expore;
+
+import com.blocklegend001.expore.blocks.ModBlocks;
+import com.blocklegend001.expore.items.ModCreativeModTabs;
+import com.blocklegend001.expore.items.ModItems;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+
+@Mod(ExpOre.MODID)
+public class ExpOre
+{
+    public static final String MODID = "expore";
+
+    public ExpOre(IEventBus modEventBus)
+    {
+        ExpOreConfig.loadConfig();
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+        ModCreativeModTabs.register(modEventBus);
+    }
+}
